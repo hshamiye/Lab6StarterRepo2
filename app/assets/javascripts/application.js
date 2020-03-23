f20a2078e7adec9e73fca922a8c74b6c70d26474
Hasan <hshamiye@andrew.cmu.edu>
2020-03-23 14:48:38 +0000

    fixed the formatting in the form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// AnyJavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
@@ -10,8 +10,23 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs 
 //= require rails-ujs
+//= require jquery
+//= require materialize-sprockets
 //= require_tree .
-
+$( document ).ready(function () {
+    $(".dropdown-button").dropdown();
+    
+    $('select').material_select();
+    
+    $('.datepicker').pickadate({
+    format: 'mmmm dd, yyyy',
+    formatSubmit: 'mmmm dd, yyyy',
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+    });
+})
